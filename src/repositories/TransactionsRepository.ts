eafa64aa11dfb4d3215ec069b753cce753cc4165
Samuel Ramos <samuelsantos7@yahoo.com.br>
2020-06-23 13:08:13 -0300

    Desafio Concluído

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -15,14 +21,38 @@ class TransactionsRepository {
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+    const income = this.transactions.reduce((total, transaction) => {
+      return transaction.type === 'income' ? total + transaction.value : total;
+    }, 0);
+    const outcome = this.transactions.reduce((total, transaction) => {
+      return transaction.type === 'outcome' ? total + transaction.value : total;
+    }, 0);
+    const total = income - outcome;
+
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 
-  public create(): Transaction {
+  public create({ type, title, value }: CreateTransactionDTO): Transaction {
     // TODO
+    const transaction = new Transaction({ value, title, type });
+    const { total } = this.getBalance();
+
+    if (transaction.type === 'outcome' && transaction.value > total) {
+      throw Error('Total amount less than the transaction amount.');
+    }
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
